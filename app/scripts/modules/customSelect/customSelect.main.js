9d2d5683db4b0846a919a46bf42b779c35c7ba24
Carrie MacDonald <carrie8783@gmail.com>
2016-07-20 17:54:21 -0400

    Adding site pages

---
+++ app/scripts/modules/customSelect/customSelect.main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// var cselect = require('jquery.customSelect');
+
+// module.exports = class customSelect{
+//   constructor($el){
+//     this.$el = $el;
+//     this.method(this.$el);
+//   }
+//   method($element){
+//     console.log($element);
+//   }
+// };
